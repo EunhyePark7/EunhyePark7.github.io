f348098a8300c8226903bac0b86e590ee465db44
EunhyePark7 <eungkong333@gmail.com>
2025-05-11 16:59:38 +0900

    chore: initial project setup

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { defineConfig } from 'vite';
+import jsconfigPaths from 'vite-jsconfig-paths';
+
+export default defineConfig({
+  plugins: [react(), jsconfigPaths()],
+  resolve: {
+    alias: [{ find: '@/', replacement: path.resolve(__dirname, './src/') }],
+  },
+});
