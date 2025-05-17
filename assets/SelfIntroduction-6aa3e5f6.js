239c0fa56255d18da06f5cce385cdc7bd8ac22c9
EunhyePark7 <EunhyePark7@users.noreply.github.com>
2025-05-17 16:34:29 +0000

    deploy: f97d7e69e2076589693dd2bf4a28a4367816bc9e

---
+++ assets/SelfIntroduction-6aa3e5f6.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-6bb58d81.js";import{A as o}from"./AppLayout-4b61caad.js";const n=()=>t.jsx(o,{children:t.jsx("h1",{children:"SelfIntroduction page"})});export{n as default};
--- assets/SelfIntroduction-f68daa1e.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-de8d0025.js";import{A as o}from"./AppLayout-db89e848.js";const n=()=>t.jsx(o,{children:t.jsx("h1",{children:"SelfIntroduction page"})});export{n as default};
